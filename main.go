ffe554231c7106cc159a107f19b83f9b52e8fd72
Joseph Antony <joeavaikath@gmail.com>
2024-12-30 20:02:26 -0500

    Middleware methods added

--- main.go
+++ main.go
@@ -2,17 +2,29 @@ package main
 
 import (
 	"net/http"
+	"sync/atomic"
+	"strconv"
 )
 
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
 func main() {
+
 	serveMux := http.NewServeMux()
 
-	serveMux.Handle("/app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
+	apiConfig := &apiConfig{}
+
+	serveMux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(http.StatusOK)
 		w.Write([]byte("OK"))
 	})
+
+	serveMux.Handle("/metrics", http.HandlerFunc(apiConfig.printMetric))
+	serveMux.Handle("/reset", http.HandlerFunc(apiConfig.resetMetric))
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -20,4 +32,19 @@ func main() {
 	}
 
 	server.ListenAndServe()
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+    return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+        cfg.fileserverHits.Add(1)
+        next.ServeHTTP(w, r)
+    })
+}
+
+func (cfg *apiConfig) printMetric(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Hits: " + strconv.Itoa(int(cfg.fileserverHits.Load()))))
+}
+
+func (cfg *apiConfig) resetMetric(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
 }
