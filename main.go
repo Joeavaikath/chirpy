32ca581d24cbc4bd7476036d8bd9891ad5bc4f9b
Joseph A <joeavaikath@gmail.com>
2025-01-01 11:53:01 -0500

    Add PostgreSQL, create user support

--- main.go
+++ main.go
@@ -1,22 +1,43 @@
 package main
 
 import (
+	"chirpy/internal/database"
+	"database/sql"
 	"encoding/json"
 	"html/template"
 	"net/http"
+	"os"
 	"strings"
 	"sync/atomic"
+	"time"
+
+	"github.com/google/uuid"
+	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 )
 
 type apiConfig struct {
 	fileserverHits atomic.Int32
+	dbQueries      *database.Queries
 }
 
 func main() {
 
+	godotenv.Load()
+
+	dbURL := os.Getenv("DB_URL")
+	db, err := sql.Open("postgres", dbURL)
+	if err != nil {
+		os.Exit(1)
+	}
+
+	dbQueries := database.New(db)
+
 	serveMux := http.NewServeMux()
 
-	apiConfig := &apiConfig{}
+	apiConfig := &apiConfig{
+		dbQueries: dbQueries,
+	}
 
 	serveMux.Handle("/app/", apiConfig.middlewareMetricsInc(http.StripPrefix("/app", http.FileServer(http.Dir(".")))))
 	serveMux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
@@ -29,6 +50,7 @@ func main() {
 	serveMux.Handle("POST /admin/reset", http.HandlerFunc(apiConfig.resetMetric))
 
 	serveMux.Handle("POST /api/validate_chirp", http.HandlerFunc(validateChirp))
+	serveMux.Handle("POST /api/users", http.HandlerFunc(apiConfig.createUser))
 
 	server := http.Server{
 		Addr:    ":8080",
@@ -59,6 +81,11 @@ func (cfg *apiConfig) printMetric(w http.ResponseWriter, r *http.Request) {
 
 func (cfg *apiConfig) resetMetric(w http.ResponseWriter, r *http.Request) {
 	cfg.fileserverHits.Store(0)
+	if os.Getenv("PLATFORM") != "dev" {
+		invalid := invalidChirp{}
+		respondWithError(w, 403, invalid)
+	}
+	cfg.dbQueries.DropUsers(r.Context())
 }
 
 type chirp struct {
@@ -75,6 +102,17 @@ type cleanedChirp struct {
 
 type invalidChirp struct {
 	Error string `json:"error"`
+}
+
+type emailRequest struct {
+	Email string `json:"email"`
+}
+
+type User struct {
+	ID        uuid.UUID `json:"id"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at"`
+	Email     string    `json:"email"`
 }
 
 func validateChirp(w http.ResponseWriter, r *http.Request) {
@@ -143,4 +181,38 @@ func sliceContains[T comparable](slice []T, item T) bool {
 		}
 	}
 	return false
+}
+
+func (cfg *apiConfig) createUser(w http.ResponseWriter, r *http.Request) {
+	params, err := decodeJSON[emailRequest](r)
+	if err != nil {
+		invalid := invalidChirp{
+			Error: "Something went wrong",
+		}
+		respondWithError(w, 500, invalid)
+	}
+	user, err := cfg.dbQueries.CreateUser(r.Context(), params.Email)
+
+	if err != nil {
+		invalid := invalidChirp{
+			Error: "Something went wrong",
+		}
+		respondWithError(w, 500, invalid)
+	}
+	userResponse := User{
+		ID:        user.ID,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+		Email:     params.Email,
+	}
+
+	respondWithJSON(w, 201, userResponse)
+
+}
+
+func decodeJSON[T any](r *http.Request) (T, error) {
+	var result T
+	decoder := json.NewDecoder(r.Body)
+	err := decoder.Decode(&result)
+	return result, err
 }
